8b69d52e3f0e1ffab68e93bb9dafd46557838322
sailesh <vermasailesh271@gmail.com>
2024-07-13 00:59:30 +0530

    fix: code refactor for app bar

---
+++ lib/screens/home_screen/widgets/home_screen_appbar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenAppBar extends StatelessWidget {
+  const HomeScreenAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CircleAvatar(
+          maxRadius: 16,
+          backgroundColor: Colors.grey,
+          child: CircleAvatar(
+            maxRadius: 14,
+            backgroundColor: Colors.white,
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              constraints: const BoxConstraints(),
+              style: const ButtonStyle(
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.person,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ),
+        const Text(
+          'Bijak',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          constraints: const BoxConstraints(),
+          style: const ButtonStyle(
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          onPressed: () {},
+          icon: const Icon(
+            Icons.logout,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
