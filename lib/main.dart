9024745e6dcd467e602d123521484d723eb8efe7
sailesh <vermasailesh271@gmail.com>
2024-07-13 16:14:10 +0530

    added river pod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:bijak_assignment/app/my_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
