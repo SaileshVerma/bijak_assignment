6ca1d1984d3c6f0a378c781137c6bc064d64f3c9
sailesh <vermasailesh271@gmail.com>
2024-07-13 13:47:26 +0530

    added product model class

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String name;
+  final String weight;
+  final String currencyUnit;
+  final double price;
+  final String description;
+
+  const Product({
+    required this.id,
+    required this.name,
+    required this.weight,
+    required this.currencyUnit,
+    required this.price,
+    required this.description,
+  });
+}
